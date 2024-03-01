bd1ba0ee1d1b071f32d7493be64d196a3a73584a
lacigabi03 <lacigabi020318@icloud.com>
2024-03-01 09:53:10 +0100

    adatb atiras

--- app/routes/sajatbackend.js
+++ app/routes/sajatbackend.js
@@ -19,7 +19,7 @@ module.exports = function(app) {
       host: 'localhost',
       user: 'root',
       password: '',
-      database: 'web_zarodoga'
+      database: 'webes_utazas_lg_tv'
     })
     
     connection.connect()
